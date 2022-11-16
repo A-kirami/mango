11fbe7a6dc21ec22e52f79f537ab693aab465f28
Akirami <66513481+A-kirami@users.noreply.github.com>
2022-11-16 19:11:32 +0800

    🐛 修复 `version` 获取问题

--- mango/__init__.py
+++ mango/__init__.py
@@ -1,14 +1,11 @@
+from importlib.metadata import version
+
 from mango.drive import connect
 from mango.expression import OPR
 from mango.fields import Field
 from mango.index import Attr, Index, Order
 from mango.models import Model
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:  # pragma: no cover
-    import importlib_metadata  # type: ignore
-
-__version__ = importlib_metadata.version(__name__)
+__version__ = version("mango-odm")
 
 __all__ = ["connect", "OPR", "Field", "Attr", "Index", "Order", "Model"]
